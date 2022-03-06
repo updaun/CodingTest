2ff1216e633e25d4fc39da35853ae1d90b6fd9e0
updaun <updauney@daum.net>
2022-03-06 19:16:29 +0900

    220306 TIL

--- boj/solution/question_10250.py
+++ boj/solution/question_10250.py
@@ -2,20 +2,16 @@ n = int(input())
 checkin_list = []
 for i in range(n):
     checkin_list.append(list(map(int, input().split(" "))))
-
 for i in checkin_list:
     H, W, P = i
-    if P%H != 0:
-        if P//H == 1:
-            print(str(P%H)+"0"+"1")
-        elif P//H < 10:
-            print(str(P%H)+"0"+str(P//H+1))
-        else:
-            print(str(P%H)+str(P//H+1))
+    floor, room_num = 1, 1
+    while P > 1:
+        floor += 1
+        if floor > H:
+            floor = 1
+            room_num += 1
+        P -= 1
+    if room_num < 10:
+        print(str(floor)+"0"+str(room_num))
     else:
-        if P//H == 1:
-            print(str(P%H)+"0"+"1")
-        if P//H < 10:
-            print("1"+"0"+str(P//H+1))
-        else:
-            print("1"+str(P//H+1))
+        print(str(floor)+str(room_num))
