605421049f8eedca40cf452a7e0ca6b42fe32b8d
updaun <updauney@daum.net>
2022-03-20 21:58:34 +0900

    220320 TIL
    백준 1002번 - 터렛 문제 풀이 : 케이스 분류 및 풀이, 내접원, 외접원 고려 및 반지름 차의 절대값 활용 필요

--- boj/solution/question_1002.py
+++ boj/solution/question_1002.py
@@ -8,14 +8,13 @@ for i in range(n):
 for t in target_list:
     x1, y1, r1, x2, y2, r2 = t
     distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
-    threshold = distance - r1 - r2
     target = 0
-    if x1 == x2 and y1 == y2 and r1 == r2:
-        target = -1
-    # elif distance <= r1 or distance <= r2:
-    #     target = 0
-    elif threshold < 0:
-        target = 2
-    elif threshold == 0 or (r1 - distance == r2) or (r2 - distance == r1):
-        target = 1
+    if x1 == x2 and y1 == y2:
+        if r1 == r2:
+            target = -1
+    else:
+        if distance == r1+r2 or distance == abs(r1-r2):
+            target = 1
+        elif distance < r1+r2 and distance > abs(r1-r2):
+            target = 2
     print(target)
