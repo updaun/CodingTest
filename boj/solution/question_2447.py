4449eb6cb807d7a6da714ccd718d923471c83b4d
updaun <updauney@daum.net>
2022-03-22 18:32:38 +0900

    220322 TIL
    백준 2447번 - 별 찍기 - 10 풀이 : 재귀 함수 응용

--- boj/solution/question_2447.py
+++ boj/solution/question_2447.py
@@ -1,6 +1,18 @@
-def star(n):
-    print("*"*n)
-    print("*"+" "+"*")
-    print("*"*n)
-    
-star(star(3))
+def star(n, x):
+    output = []
+    if n == 3:
+        return x
+    else:
+        for i in x:
+            output.append(i*3)
+        for i in x:
+            output.append(i+" "*len(x)+i)
+        for i in x:
+            output.append(i*3)
+    return star(n//3, output)
+
+n = int(input())
+start = ["***", "* *", "***"]
+result = star(n, start)
+for i in result:
+    print(i)
