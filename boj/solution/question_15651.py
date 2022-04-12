51e1f182a6f6f064375bddfcd31f7c6987e933b9
updaun <updauney@daum.net>
2022-04-12 14:25:28 +0900

    220412 TIL
    백준 15651번 - N과 M(3) : 백트레킹 기본 예제, 완전탐색

---
+++ boj/solution/question_15651.py
@@ -0,0 +1,14 @@
+n, m = tuple(map(int, input().split()))
+
+n_list = []
+
+def n_search():
+    if len(n_list) == m:
+        print(" ".join(map(str, n_list)))
+        return
+    for i in range(1, n+1):
+        n_list.append(i)
+        n_search()
+        n_list.pop()
+
+n_search()
