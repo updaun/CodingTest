5f7347cc374830cdd9b54b87e6f42f80d2143235
updaun <updauney@daum.net>
2022-05-09 23:06:16 +0900

    220509 TIL
    백준 1149번 - RGB 거리 풀이 : DP 규칙 찾기가 어려움, 타인 풀이 참조

--- boj/solution/question_1149.py
+++ boj/solution/question_1149.py
@@ -1,40 +1,14 @@
 import sys
 
 n = int(sys.stdin.readline())
-cost_list = []
-total_list = []
-# for i in range(3):
-# rgb_list = list(map(int, sys.stdin.readline().split()))
-# cost = min(rgb_list)
-# index = rgb_list.index(min(rgb_list))
+rgb_list = []
+
 for i in range(n):
-    rgb_list = list(map(int, sys.stdin.readline().split()))
-    total_list.append(rgb_list)
+    rgb_list.append(list(map(int, sys.stdin.readline().split())))
 
-for i in range(3):
-    cost = rgb_list[i]
-    index = i
-    for rgb_list in total_list[1:]:
-        now_index = rgb_list.index(min(rgb_list))
-        if index != now_index:
-            cost += min(rgb_list)
-            index = now_index
-        else:
-            if now_index == 0:
-                cost += min(rgb_list[1:])
-                now_index = rgb_list[1:].index(min(rgb_list[1:]))
-                index = now_index + 1
-            elif now_index == 1:
-                cost += min([rgb_list[0],rgb_list[2]])
-                now_index = [rgb_list[0],rgb_list[2]].index(min([rgb_list[0],rgb_list[2]]))
-                if now_index == 0: 
-                    index = 0
-                else: index = 2
-            else:
-                cost += min(rgb_list[:2])
-                now_index = rgb_list[:2].index(min(rgb_list[:2]))
-                index = now_index
-    cost_list.append(cost)
-print(cost_list)
+for i in range(1, n):
+    rgb_list[i][0] = rgb_list[i][0] + min(rgb_list[i-1][1], rgb_list[i-1][2])
+    rgb_list[i][1] = rgb_list[i][1] + min(rgb_list[i-1][0], rgb_list[i-1][2])
+    rgb_list[i][2] = rgb_list[i][2] + min(rgb_list[i-1][0], rgb_list[i-1][1])
 
-print(min(cost_list))
+print(min(rgb_list[n-1]))
