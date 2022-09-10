b607589849ba349293a62c38e5cce2236dc8aa4b
updaun <updauney@daum.net>
2022-09-10 18:45:02 +0900

    220910 TIL
    백준 10972,10973번 - 다음 순열, 이전 순열 풀이 : 기존에 알지 못했던 문법을 많이 알게된 코드

---
+++ boj/solution/question_10973.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+temp = -1
+
+for i in range(n-1, 0, -1):
+    if arr[i-1] > arr[i]:
+        temp = i-1
+        break
+else:
+    print(-1)
+    sys.exit()
+
+for i in range(n-1, 0, -1):
+    if arr[temp] > arr[i]:
+        arr[temp], arr[i] = arr[i], arr[temp]
+        arr = arr[:temp+1]+sorted(arr[temp+1:], reverse=True)
+        print(*arr)
+        break
