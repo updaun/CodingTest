841a64a1bd2cd759c7ac8cb75442a3ee80ba887b
updaun <updauney@daum.net>
2022-09-16 23:51:33 +0900

    220916 TIL
    백준 9663번 - N-Queen 풀이 추가 : 백트래킹에서 효율성을 고려한 풀이

--- boj/solution/question_9663.py
+++ boj/solution/question_9663.py
@@ -1,3 +1,4 @@
+# 30968ms
 import sys
 def n_queens(i, col):
     global count
@@ -23,4 +24,47 @@ n = int(sys.stdin.readline())
 col = [0] * (n+1)
 count = 0
 n_queens(0, col)
+print(count)
+
+# 시간 초과
+# n = int(input())
+# count = 0
+# arr = []
+# def backtracking():
+#     global count
+#     if len(arr) == n:
+#         for i in range(n):
+#             for j in range(1, n-i):
+#                 if abs(arr[i]-arr[i+j])==j:
+#                     return
+#         count += 1
+#         return
+    
+#     for i in range(n):
+#         if i not in arr:
+#             arr.append(i)
+#             backtracking()
+#             arr.pop()
+
+# backtracking()
+# print(count)
+
+# 5100ms
+n = int(input())
+count = 0
+a, b, c = [False]*n, [False]*(2*n-1), [False]*(2*n-1)
+
+def backtracking(i):
+    global count
+    if i == n:
+        count += 1
+        return
+    
+    for j in range(n):
+        if not (a[j] or b[i+j] or c[i-j+n-1]):
+            a[j] = b[i+j] = c[i-j+n-1] = True
+            backtracking(i+1)
+            a[j] = b[i+j] = c[i-j+n-1] = False
+
+backtracking(0)
 print(count)
