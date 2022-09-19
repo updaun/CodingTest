3cc11ab611bb8dad9e19e15a3bb2734ca52c16ef
updaun <updauney@daum.net>
2022-09-20 00:33:31 +0900

    220920 TIL
    백준 11047번 - 동전 0 풀이 : 풀이 개선 재풀이, 속도 개선, 코드 단축

--- boj/solution/question_11047.py
+++ boj/solution/question_11047.py
@@ -1,14 +1,27 @@
+# 속도개선 68ms
 import sys
-n, k = list(map(int, sys.stdin.readline().split()))
-coin_list = []
-for i in range(n):
-    coin_list.append(int(sys.stdin.readline()))
-
+input = sys.stdin.readline
+c_num, value = map(int, input().split())
+coin_list = [int(input()) for i in range(c_num)]
 count = 0
-for i in coin_list[::-1]:
-    if k >= i:
-        count += k//i
-        k = k%i
-    if k == 0:
-        break
+for c in coin_list[::-1]:
+    if c <= value:
+        temp, value = divmod(value, c)
+        count += temp
 print(count)
+
+# 72ms
+# import sys
+# n, k = list(map(int, sys.stdin.readline().split()))
+# coin_list = []
+# for i in range(n):
+#     coin_list.append(int(sys.stdin.readline()))
+
+# count = 0
+# for i in coin_list[::-1]:
+#     if k >= i:
+#         count += k//i
+#         k = k%i
+#     if k == 0:
+#         break
+# print(count)
