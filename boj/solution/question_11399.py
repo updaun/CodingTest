dae604023880af7bbab8bd478649f3829a038133
updaun <updauney@daum.net>
2022-09-21 01:10:34 +0900

    220921 TIL
    백준 11399번 - ATM 풀이 : 그리디 기초, 풀이 개선

--- boj/solution/question_11399.py
+++ boj/solution/question_11399.py
@@ -1,8 +1,15 @@
+# 신규 풀이 68ms
 import sys
-n = int(sys.stdin.readline())
-result = 0
-raw = sorted(list(map(int, sys.stdin.readline().split())))
-for i in range(len(raw)):
-    result += sum(raw[:i+1])
-print(result)
+input = sys.stdin.readline
+n = int(input())
+n_list = sorted(list(map(int, input().split())))
+print(sum([n_list[i]*(n-i) for i in range(n)]))
 
+# 기존 풀이 72ms
+# import sys
+# n = int(sys.stdin.readline())
+# result = 0
+# raw = sorted(list(map(int, sys.stdin.readline().split())))
+# for i in range(len(raw)):
+#     result += sum(raw[:i+1])
+# print(result)
