e10f68a180a09ac95bae2c2009177ab91218aa47
updaun <updauney@daum.net>
2022-09-25 20:57:50 +0900

    220925 TIL
    백준 10816번 - 숫자 카드 2 풀이 : 딕셔너리를 활용한 풀이가 빠른 풀이 방법

--- boj/solution/question_10816.py
+++ boj/solution/question_10816.py
@@ -9,3 +9,22 @@ for i in list(map(int, sys.stdin.readline().split())):
 M = int(sys.stdin.readline())
 question = list(map(int, sys.stdin.readline().split()))
 print(" ".join(map(str, [card_dict[i] if i in card_dict.keys() else 0 for i in question])))
+
+# 220924 신규 풀이 조금 더 느려짐
+import sys
+input = sys.stdin.readline
+n = int(input())
+n_dict = {}
+for i in list(map(int, input().split())):
+    if i in n_dict.keys():
+        n_dict[i] += 1
+    else:
+        n_dict[i] = 1
+m = int(input())
+result = []
+for i in list(map(int, input().split())):
+    if i in n_dict.keys():
+        result.append(n_dict[i])
+    else:
+        result.append(0)
+print(*result)
