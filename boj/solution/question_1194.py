0eb2c38eb4c3e73d3f6dab84051650124a2a10fa
updaun <updauney@daum.net>
2022-10-30 20:31:57 +0900

    221030 TIL
    백준 1194 - 달이 차오른다 가자 풀이 : 다시 풀어보기

--- boj/solution/question_1194.py
+++ boj/solution/question_1194.py
@@ -1,22 +1,22 @@
+# 고민하다가 블로그 찾아봄
+# https://pacific-ocean.tistory.com/400
+
 from collections import deque
 import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
-s = []
 q = deque()
-visit = [[[0] * 64 for i in range(m)] for i in range(n)]
+s = []
+visit = [[[0]*64 for i in range(m)] for i in range(n)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, 1, -1]
 
 def bfs():
-
     while q:
-        
-        x,y,c,cnt = q.popleft()
+        x, y, c, cnt = q.popleft()
 
         for i in range(4):
-
             nx = x + dx[i]
             ny = y + dy[i]
 
@@ -28,22 +28,24 @@ def bfs():
                     return cnt + 1
                 else:
                     if s[nx][ny].isupper():
-                        if c & 1 << (ord(s[nx][ny]) - 65):
+                        if c & 1 << (ord(s[nx][ny])-65):
                             visit[nx][ny][c] = 1
                             q.append([nx, ny, c, cnt+1])
                     elif s[nx][ny].islower():
-                        nc = c | (1 << ord(s[nx][ny]) - 97)
+                        nc = c | 1 << (ord(s[nx][ny])-97)
                         if visit[nx][ny][nc] == 0:
                             visit[nx][ny][nc] = 1
-                            q.append([nx, ny, nc, cnt + 1])
+                            q.append([nx, ny, nc, cnt+1])
+
     return -1
 
 for i in range(n):
     a = list(input().strip())
     s.append(a)
     for j in range(m):
-        if a[j] == '0':
+        if a[j] == "0":
             q.append([i, j, 0, 0])
             s[i][j] = "."
             visit[i][j][0] = 1
+
 print(bfs())
