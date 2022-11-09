e50aa5aed71849227d16ff9ea64357afc016447a
updaun <updauney@daum.net>
2022-11-09 22:23:45 +0900

    221109 TIL
    백준 1094번 풀이 업데이트 - 비트마스킹 풀이 및 이진수 변환 풀이

--- boj/solution/question_1094.py
+++ boj/solution/question_1094.py
@@ -1,3 +1,17 @@
+# 비트 마스킹 풀이(68ms)
+n = int(input())
+bit = 0
+while n > 0:
+    n &= (n-1)
+    bit += 1
+print(bit)
+
+# 이진수 변환 후 1 개수 세기(76ms)
+n = int(input())
+print(bin(n)[2:].count("1"))
+
+
+# combinations 풀이(76ms)
 from itertools import combinations
 n = int(input())
 s_list = [64]
