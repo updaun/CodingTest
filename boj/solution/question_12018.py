9010aa6ad1e84ed340d816abde8d91ed0e02aa65
updaun <updauney@daum.net>
2022-11-11 19:18:14 +0900

    221111 TIL
    백준 12018번 - Yonsei TOTO 풀이 : 정렬 방향 변경으로 풀이 성공

--- boj/solution/question_12018.py
+++ boj/solution/question_12018.py
@@ -4,15 +4,16 @@ n, m = map(int, input().split())
 b_list = []
 for _ in range(n):
     p, i = map(int, input().split())
-    p_list = sorted(list(map(int, input().split())))
+    p_list = sorted(list(map(int, input().split())), reverse=True)
     try:
         b_list.append(p_list[i-1])
     except:
         b_list.append(1)
 b_list.sort()
 for i in range(len(b_list)):
-    if m <= 0:
-        result = i
-        break
     m -= b_list[i]
-print(result)
+    if m < 0:
+        print(i)
+        break
+else:
+    print(len(b_list))
