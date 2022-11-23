2141480bf1ce3d57579b0b2e579efa94867e481c
updaun <updauney@daum.net>
2022-11-23 18:57:48 +0900

    221123 TIL
    뭐가 더 빠를지 비교 실험

--- boj/solution/question_10865.py
+++ boj/solution/question_10865.py
@@ -1,3 +1,17 @@
+# 리스트를 활용한 풀이
+# 메모리 31552KB, 시간 968ms
+import sys
+n,m=map(int,sys.stdin.readline().split())
+cnt=[0]*(n+1)
+for i in range(m):
+    a,b=map(int,sys.stdin.readline().split())
+    cnt[a]+=1
+    cnt[b]+=1
+for i in range(1,n+1):
+    print(cnt[i])
+
+# 딕셔너리를 활용한 풀이
+# 메모리 118392KB, 시간 1556ms
 import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
