3db5609f420179b996645fc1a4ffdc27583c71f3
updaun <updauney@daum.net>
2022-12-09 20:22:31 +0900

    221209 TIL
    백준 1598번 - 꼬리를 무는 숫자 나열 풀이 : 나누어 떨어지는 것에 대한 추가 처리

--- boj/solution/question_1598.py
+++ boj/solution/question_1598.py
@@ -1,4 +1,10 @@
 m,n = map(int, input().split())
 x1,y1 = divmod(m, 4)
 x2,y2 = divmod(n, 4)
+if y1 == 0:
+    x1 -= 1
+    y1 = 4
+if y2 == 0:
+    x2 -= 1
+    y2 = 4
 print(abs(x1-x2)+abs(y1-y2))
