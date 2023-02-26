65b4d9abaeefcb1e41a147eb9023cea13905f6ac
updaun <updauney@daum.net>
2023-02-26 23:47:11 +0900

    230226 TIL
    프로그래머스 - 문제풀이 : 문제를 이해하는 과정에서 잘못 이해, 시간 누적 계산 필요

---
+++ programmers/230226_1.py
@@ -0,0 +1,52 @@
+# 프로그래머스
+# 코딩테스트 연습 > 2022 KAKAO BLIND RECRUITMENT > 주차 요금 계산
+# https://school.programmers.co.kr/learn/courses/30/lessons/92341
+
+import math
+
+
+def solution(fees, records):
+    answer = dict()
+    b, d, p, f = fees
+    s = dict()
+    for recode in records:
+        t, n, io = recode.split()
+
+        if io == "IN":
+            s[n] = t
+        else:
+            ih, im = map(int, s[n].split(":"))
+            oh, om = map(int, t.split(":"))
+            time = (oh*60+om) - (ih*60+im)
+
+            if n not in answer.keys():
+                answer[n] = time
+            else:
+                answer[n] += time
+            s.pop(n)
+
+    while s:
+        n, t = s.popitem()
+        ih, im = map(int, t.split(":"))
+        time = (23*60+59) - (ih*60+im)
+
+        if n not in answer.keys():
+            answer[n] = time
+        else:
+            answer[n] += time
+    return [d if answer[i]-b <= 0 else d+math.ceil((answer[i]-b)/p)*f for i in sorted(answer)]
+
+
+q = solution([180, 5000, 10, 600], ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT",
+             "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"])
+assert q == [14600, 34400, 5000], "불일치"
+print(q)
+
+q = solution([120, 0, 60, 591], ["16:00 3961 IN", "16:00 0202 IN",
+                                 "18:00 3961 OUT", "18:00 0202 OUT", "23:58 3961 IN"])
+assert q == [0, 591], "불일치"
+print(q)
+
+q = solution([1, 461, 1, 10], ["00:00 1234 IN"])
+assert q == [14841], "불일치"
+print(q)
