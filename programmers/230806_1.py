08dce4ba06f09bd895d38e84de27601686d557b6
updaun <updauney@daum.net>
2023-08-06 20:07:43 +0900

    230806 TIL
    프로그래머스 - 문제 풀이
    - Counter를 활용한 중복제거

---
+++ programmers/230806_1.py
@@ -0,0 +1,19 @@
+# 프로그래머스
+# 코딩테스트 연습 > 코딩 기초 트레이닝 > 무작위로 K개의 수 뽑기
+# https://school.programmers.co.kr/learn/courses/30/lessons/181858
+
+from collections import Counter
+
+def solution(arr, k):
+    answer = list(Counter(arr).keys())[:k]
+    if len(answer) < k:
+        answer += [-1] * (k-len(answer))
+    return answer
+
+q = solution([0, 1, 1, 2, 2, 3], 3)
+assert q == [0, 1, 2], "불일치"
+print(q)
+
+q = solution([0, 1, 1, 1, 1], 4)
+assert q == [0, 1, -1, -1], "불일치"
+print(q)
