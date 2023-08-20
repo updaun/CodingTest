0ed10abf8990e40af2158479b616b2efa462f020
updaun <updauney@daum.net>
2023-08-20 23:50:07 +0900

    230820 TIL
    프로그래머스 - 문제 풀이
    - 팩토리얼 라이브러리

---
+++ groom/230820_1.py
@@ -0,0 +1,21 @@
+# 구름LEVEL
+# 큰 팩토리얼
+# https://level.goorm.io/exam/175880/%ED%81%B0-%ED%8C%A9%ED%86%A0%EB%A6%AC%EC%96%BC/quiz/1
+
+# 팩토리얼을 사용하는 가장 빠른 방법?
+import math
+n = int(input())
+print(math.factorial(n) % 1000000007)
+
+# 예시1
+# 입력
+# 3
+# 출력
+# 6
+
+
+# 예시2
+# 입력
+# 100
+# 출력
+# 437918130
