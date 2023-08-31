69831d35dcdebb65102a105788030aeb7a25d508
updaun <updauney@daum.net>
2023-08-31 23:50:15 +0900

    230831 TIL
    구름LEVEL - 문제 풀이
    - 문자열 수식 계산 eval()

---
+++ groom/230831_1.py
@@ -0,0 +1,18 @@
+# 구름LEVEL
+# 큰 수식 찾기
+# https://level.goorm.io/exam/174717/%ED%81%B0-%EC%88%98%EC%8B%9D-%EC%B0%BE%EA%B8%B0/quiz/1
+
+n = input().split()
+print(max([eval(i) for i in n]))
+
+# 10+5 10+6
+
+# 16
+
+# 10*3-10 10*4-50
+
+# 20
+
+# 10*10-99 99-10*10
+
+# 1
