c12486694cbb139037be6c216df2f682aefe1c93
updaun <updauney@daum.net>
2023-09-07 12:58:29 +0900

    230907 TIL
    구름LEVEL - 문제 풀이
    - 규칙 찾기, 점화식

---
+++ groom/230907_1.py
@@ -0,0 +1,18 @@
+# 구름LEVEL
+# RGB 주차장
+# https://level.goorm.io/exam/177463/rgb-%EC%A3%BC%EC%B0%A8%EC%9E%A5/quiz/1
+
+result = 3
+n = int(input())
+for i in range(n-1):
+	result *= 2
+print(result % 100000007)
+			
+
+# 3
+
+# 12
+
+# 100
+
+# 27122210
