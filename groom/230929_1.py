ac41edc2bde45f8d0656dec8482f4b5c6145f5be
updaun <updauney@daum.net>
2023-09-29 20:24:02 +0900

    230929 TIL
    구름LEVEL - 문제 풀이
    - 그리디의 기본
    - 최소 동작

---
+++ groom/230929_1.py
@@ -0,0 +1,19 @@
+# 구름LEVEL
+# 통증
+# https://level.goorm.io/exam/195690/%ED%86%B5%EC%A6%9D/quiz/1
+
+n = int(input())
+count = 0
+t = [14, 7]
+for i in t:
+	a, n = divmod(n, i)
+	count += a
+print(count+n)
+	
+# 8
+
+# 2
+
+# 100
+
+# 9
