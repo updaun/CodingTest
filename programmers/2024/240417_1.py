ea3a89e940cc0826b4ee132d64b0b9ac0c267dd0
updaun <updauney@daum.net>
2024-04-18 23:46:51 +0900

    230418 TIL
    프로그래머스 - 문제 풀이
    스택 문제 풀이 진행중, 조금 더 개선된 코드

--- programmers/2024/240417_1.py
+++ programmers/2024/240417_1.py
@@ -6,20 +6,25 @@ from collections import deque
 
 def solution(order):
     answer = []
-    origin = deque(list(range(1, len(order)+1)))
+    origin = deque(range(1, len(order)+1))
     stack = []
+    temp = 0
     for i in order:
+        temp += 1
         if stack and stack[-1] == i:
             answer.append(stack.pop())
-        if origin and stack and i != origin[0] and i != stack[-1]:
+        if origin and origin[0] == i:
+            answer.append(origin.popleft())
+
+        while origin and origin[0] != i:
+            stack.append(origin.popleft())
+        else:
+            if origin:
+                answer.append(origin.popleft())
+
+        if temp > len(answer):
             break
-        while origin:
-            temp = origin.popleft()
-            if temp != i:
-                stack.append(temp)
-            else:
-                answer.append(i)
-                break
+
     return len(answer)
 
 q = solution([4, 3, 1, 2, 5])
