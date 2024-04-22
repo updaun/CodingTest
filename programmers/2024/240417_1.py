57bd8dad66a7e78c7d1ebe01f7478b36cd64c571
updaun <updauney@daum.net>
2024-04-22 23:32:50 +0900

    230422 TIL
    프로그래머스 - 문제 풀이
    스택 문제 풀이, 다른 사람의 풀이 참고

--- programmers/2024/240417_1.py
+++ programmers/2024/240417_1.py
@@ -2,30 +2,20 @@
 # 코딩테스트 연습 > 연습문제 > 택배상자
 # https://school.programmers.co.kr/learn/courses/30/lessons/131704?language=python3
 
-from collections import deque
-
 def solution(order):
-    answer = []
-    origin = deque(range(1, len(order)+1))
+    answer = 0
+    i = 1
     stack = []
-    temp = 0
-    for i in order:
-        temp += 1
-        if stack and stack[-1] == i:
-            answer.append(stack.pop())
-        if origin and origin[0] == i:
-            answer.append(origin.popleft())
 
-        while origin and origin[0] != i:
-            stack.append(origin.popleft())
-        else:
-            if origin:
-                answer.append(origin.popleft())
-
-        if temp > len(answer):
-            break
-
-    return len(answer)
+    while i != len(order) + 1:
+        stack.append(i)
+        while stack[-1] == order[answer]:
+            answer += 1
+            stack.pop()
+            if len(stack) == 0:
+                break
+        i += 1
+    return answer
 
 q = solution([4, 3, 1, 2, 5])
 assert q == 2, "불일치"
